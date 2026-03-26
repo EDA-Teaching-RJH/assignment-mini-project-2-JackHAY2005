cfdd05aecbc937ac8a9d26e1819647bdcab8da23
JackHAY2005 <jch12@kent.ac.uk>
2026-03-26 19:54:37 +0000

    Display Menu

--- Code.py
+++ Code.py
@@ -1,3 +1,16 @@
 i = ["Messi", "Ronaldo", "Neymar", "Bale", "Suarez"]
 p = ["RW", "ST", "LW", "CM", "CAM", "GK", "CB", "LB", "RB", "CDM"]
 n = ["Argentina", "Portugal", "Brazil", "Wales", "Uruguay"]
+
+def display_menu():
+    print("\n--- MENU ---")
+    print("1. View Database")
+    print("2. Add Player")
+    print("3. Remove Player")
+    print("4. Update Position")
+    print("5. Search Player")
+    print("6. Filter by Nationality")
+    print("7. Calculate Total Value")
+    print("9. Exit")
+display_menu()
+#
